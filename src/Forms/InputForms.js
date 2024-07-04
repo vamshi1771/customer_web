5f93d8a83c68fd38a344ae9bf1c5eb3059ee95f0
vamshi1771 <122960565+vamshi1771@users.noreply.github.com>
2024-07-04 19:24:23 +0530

    Bug fixes

--- src/Forms/InputForms.js
+++ src/Forms/InputForms.js
@@ -1,47 +1,58 @@
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import Form from 'react-bootstrap/Form';
-import '../index.css';
-import '../App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "react-bootstrap/Form";
+import "../index.css";
+import "../App.css";
 import { Snackbar } from "@mui/material";
-import Button from '@mui/material/Button';
-import Alert from '@mui/material/Alert';
-import { openSnackBar,closeSnackBar } from "../redux/actions/snackbaractions";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import { openSnackBar, closeSnackBar } from "../redux/actions/snackbaractions";
 import { Customer_Registered } from "../variables-urls/Variables";
-import {useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Label } from "recharts";
 
 function Postforms(props) {
-
   const [CustomerName, setCustomerName] = useState("");
   const [CustomerId, setCustomerId] = useState("");
   const [Region, setRegion] = useState("");
   const [gender, setgender] = useState("");
 
- const disPatch=useDispatch();
- const open = useSelector((state)=> state.snackbar);
- console.log(open);
-  
-  const [errorMessage, setErrorMessage] = useState('');
+  const initialState = {
+    CustomerName: "",
+    Region: "",
+    gender: "",
+    phoneNumber: "",
+    Address: "",
+  };
+  const [customerDetails, setCustomerDetails] = React.useState(initialState);
+
+  const disPatch = useDispatch();
+  const open = useSelector((state) => state.snackbar);
+  console.log(open);
+
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     const Customerdetails = {
-      "customerId": CustomerId,
-      "customerName": CustomerName,
-      "region": Region,
-      "gender": gender,
-    }
+      phoneNumber: customerDetails.phoneNumber,
+      customerName: customerDetails.CustomerName,
+      region: customerDetails.Region,
+      gender: customerDetails.gender,
+    };
 
+    console.log("Customerdetails", Customerdetails);
+    debugger;
     fetch(`http://localhost:8080/Post`, {
-      method: 'POST',
-      headers: { 'Content-type': 'Application/Json' },
-      body: JSON.stringify(Customerdetails)
+      method: "POST",
+      headers: { "Content-type": "Application/Json" },
+      body: JSON.stringify(Customerdetails),
     })
       .then((res) => res.text())
       .then((data) => {
-        setErrorMessage(data)
-        disPatch(openSnackBar({ message: data, type: 'success' }));
+        setErrorMessage(data);
+        disPatch(openSnackBar({ message: data, type: "success" }));
       })
       .catch((err) => console.log(err));
   }
@@ -52,79 +63,161 @@ function Postforms(props) {
     disPatch(closeSnackBar());
   };
 
+  const handleChange = (key, value) => {
+    console.log("value", value);
+    setCustomerDetails({
+      ...customerDetails,
+      [key]: value,
+    });
+  };
+
   const handleNameChange = (e) => {
     const search = e.target.value;
     const ans = search.trim();
     const str = ans.split(" ");
     let temp = "";
-    for (let num = 0; num < ((str.length)); num++) {
+    for (let num = 0; num < str.length; num++) {
       if (str[num] === "") continue;
       else temp = temp + str[num] + " ";
     }
     temp = temp.trim();
     setCustomerName(temp);
-  }
-
-
-
+  };
 
   return (
-    <div  >
-      <Form className="PostCustomerForm" style={{ display: 'grid', 'grid-template-columns': "1fr 1fr", 'grid-template-rows': "1fr 1fr", gap: '50px', 'font-weight': '500', 'font-size': 'large', 'margin-top': '50px' }} onSubmit={handleSubmit}>
+    <div>
+      <Form
+        className="PostCustomerForm"
+        style={{
+          display: "grid",
+          "grid-template-columns": "1fr 1fr",
+          "grid-template-rows": "1fr 1fr",
+          gap: "50px",
+          "font-weight": "500",
+          "font-size": "large",
+          "margin-top": "50px",
+        }}
+        onSubmit={handleSubmit}
+      >
         <Form.Group>
-          <Form.Label id="formName">CustomerName:</Form.Label>
-          <Form.Control className="required-field" required type="text"
-            placeholder="Enter CustomerName" name="customername" onChange={handleNameChange} />
+          <label className="d-block mb-1">Customer Name: </label>
+          <input
+            className="cm-input-field"
+            required
+            type="text"
+            placeholder="Enter CustomerName"
+            name="customername"
+            value={customerDetails.CustomerName}
+            onChange={(value)=>{
+              handleChange("customerName", value)}}
+          />
+        </Form.Group>
+        <Form.Group>
+          <label className="d-block mb-1">Region: </label>
+          <input
+            className="cm-input-field"
+            required
+            type="text"
+            placeholder="Enter region"
+            name="region"
+            value={customerDetails.Region}
+            onChange={(e) => {
+              handleChange("Region", e.target.value);
+            }}
+          />
         </Form.Group>
 
         <Form.Group>
-          <Form.Label id="formName">Enter CustomerId<span className="requiredfield">*</span>:</Form.Label>
-          <Form.Control className="has-suffix" required type="number"
-            placeholder="Enter customerId" name="customerid" onChange={e => setCustomerId(e.target.value)} ></Form.Control>
+          <Form.Label id="formName">Region:</Form.Label>
+          <Form.Control
+            className="required-field"
+            required
+            type="text"
+            placeholder="Enter Region"
+            name="region"
+            onChange={(e) => {
+              handleChange("Region", e.target.value);
+            }}
+          />
         </Form.Group>
 
         <Form.Group>
-          <Form.Label id="formName">Enter Region:</Form.Label>
-          <Form.Control className="required-field" required type="text" placeholder="Enter Region" name="region" onChange={e => setRegion(e.target.value)} />
+          <Form.Label id="formName">Gender:</Form.Label>
+          <Form.Control
+            className="required-field"
+            required
+            type="text"
+            placeholder="Enter gender"
+            name="gender"
+            onChange={(e) => {
+              handleChange("Gender", e.target.value);
+            }}
+          />
         </Form.Group>
 
         <Form.Group>
-          <Form.Label id="formName">Enter gender:</Form.Label>
-          <Form.Control className="required-field" required type="text" placeholder="Enter gender" name="gender" onChange={e => setgender(e.target.value)} />
+          <Form.Label id="formName">
+            phone Number:<span className="requiredfield">*</span>:
+          </Form.Label>
+          <Form.Control
+            className="has-suffix"
+            required
+            type="text"
+            placeholder="Enter Phone Number"
+            name="customerid"
+            onChange={(e) => {
+              handleChange("phoneNumber", e.target.value);
+            }}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label id="formName">
+            Address:<span className="requiredfield">*</span>:
+          </Form.Label>
+          <Form.Control
+            className="has-suffix"
+            required
+            type="text"
+            placeholder="Enter Address"
+            name="customerid"
+            onChange={(e) => {
+              handleChange("Address", e.target.value);
+            }}
+          ></Form.Control>
         </Form.Group>
         <br></br>
 
-        <Button className="PostCustomerButton" style={{ display: 'block', alignSelf: 'center' }} id="formButton" type="submit"
-          disabled={CustomerId ? false : true}>
+        <Button
+          className="PostCustomerButton"
+          style={{ display: "block", alignSelf: "center" }}
+          id="formButton"
+          type="submit"
+          disabled={CustomerId ? false : true}
+        >
           Post customer details
         </Button>
 
-
-
-        <Snackbar open={open.status} autoHideDuration={4000} onClose={handleToClose}>
+        <Snackbar
+          open={open.status}
+          autoHideDuration={4000}
+          onClose={handleToClose}
+        >
           <Alert
             onClose={handleToClose}
-            severity={(open.message) ? "error" : "success"}
-            
+            severity={open.message ? "error" : "success"}
             variant="filled"
-            sx={{ width: '100%' }}
+            sx={{ width: "100%" }}
           >
             {errorMessage ? open.message : Customer_Registered}
           </Alert>
         </Snackbar>
       </Form>
-
-
-
-
     </div>
-  )
+  );
 }
 
 export default Postforms;
-
-
-
 
 // import React, { useState } from "react";
 // import 'bootstrap/dist/css/bootstrap.css';
@@ -137,16 +230,12 @@ export default Postforms;
 // import Button from '@mui/material/Button';
 // import Alert from '@mui/material/Alert';
 
-
-
-
 // function Postforms(Props) {
 
 //   const [CustomerName, setCustomerName] = useState("");
 //   const [CustomerId, setCustomerId] = useState("");
 //   const [Region, setRegion] = useState("");
 //   const [gender, setgender] = useState("");
-
 
 //   const [Open, setOpen] = useState(false);
 //   const open = useSelector((state) => (state.Snackbar));
@@ -175,10 +264,6 @@ export default Postforms;
 //       .catch((err) => console.log(err));
 //   }
 
-
-
-
-
 //   const handleToClose = (event, reason) => {
 //     if (reason === "clickaway") return;
 
@@ -192,8 +277,6 @@ export default Postforms;
 //     // disPatch(SNACK_OPEN());
 //   };
 
-
-
 //   const handleNameChange = (e) => {
 //     const search = e.target.value;
 //     const ans = search.trim();
@@ -206,9 +289,6 @@ export default Postforms;
 //     temp = temp.trim();
 //     setCustomerName(temp);
 //   }
-
-
-
 
 //   return (
 //     <div >
@@ -241,7 +321,6 @@ export default Postforms;
 //           Post customer details
 //         </Button>
 
-
 //         <Snackbar open={Open} autoHideDuration={6000} onClose={handleToClose}>
 //           <Alert
 //             onClose={handleToClose}
@@ -255,22 +334,8 @@ export default Postforms;
 //         </Snackbar>
 //       </Form>
 
-
-
-
 //     </div>
 //   );
 
 // }
 // export default Postforms;
-
-
-
-
-
-
-
-
-
-
-
